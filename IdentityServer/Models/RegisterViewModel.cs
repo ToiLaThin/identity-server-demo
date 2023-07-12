c32295057b846a31da1bdbed96ac6b6f58412b30
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-07-12 07:48:51 +0700

    config mvc identity server proj to login register

---
+++ IdentityServer/Models/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityServer.Models
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirmed { get; set; }
+    }
+}
